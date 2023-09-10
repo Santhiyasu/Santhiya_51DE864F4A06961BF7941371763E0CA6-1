4ae0c6042b9a5fbaf0887bb3cf3d258f998a1fc1
Santhiya077 <144580114+Santhiya077@users.noreply.github.com>
2023-09-10 11:15:16 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,15 @@
+#python program to find the factorial of a number provided by the user.
+#change the value for a different result
+num=7
+# to take input from the user
+#num = int (input("enter a number:"))
+factorial = 1
+# check if the number is nagative,positive or zero
+if num < 0:
+  print("sorry, factorial does not exist for nagative numbers")
+elif num == 0:
+  print("the factorial of 0 is 1")
+else:
+  for i in range(1,num + 1):
+    factorial = factorial*i
+    print("the factorial of",num,"is",factorial)
